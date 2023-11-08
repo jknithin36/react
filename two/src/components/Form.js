05a28c7fa36dda6c66ee76056737d693cdf482f3
unknown <jknithin36@gmail.com>
2023-11-08 13:01:35 +0530

    eatsplit

---
+++ two/src/components/Form.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    onAddItems(newItem);
+    console.log(newItem);
+    setDescription("");
+    setQuantity(1);
+  }
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What you need for your trip</h3>
+      <select value={quantity} onChange={(e) => setQuantity(+e.target.value)}>
+        {/* Array.from method creates a array from 1 to 20 and then we are looping that array using map Method */}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
