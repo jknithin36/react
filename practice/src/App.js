7a67aa1de1aa72d57712b8547985fe8d7471a82e
unknown <jknithin36@gmail.com>
2023-11-09 18:28:40 +0530

    added intialPop

---
+++ practice/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <ul>
+        <li>Logo</li>
+        <li>Home</li>
+        <li>stats</li>
+        <li>stories</li>
+        <li>Get in Touch</li>
+      </ul>
+    </div>
+  );
+}
